4c3a1970dbcf5c671a3f7f0ed56daa2646a61cd1
Siwat684259031 <684259031@webmail.npru.ac.th>
2026-03-09 11:44:02 +0700

    update inex_update_delete

--- updateCustomerForm_stu.php
+++ updateCustomerForm_stu.php
@@ -22,8 +22,9 @@
 <html lang="en">
 
 <head>
-<meta charset="UTF-8">
-<title>Update Customer</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>updatecustomer.php</title>
 </head>
 
 <body>
